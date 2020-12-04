766852e2fde9613bd9752e75ef402e5d7ba99c3b
Joaquín Matías Vacca <jvacca@santafe.gov.ar>
2020-12-03 23:32:31 -0300

    Hotfix Disponibilidad Type

--- src/Entity/Disponibilidad.php
+++ src/Entity/Disponibilidad.php
@@ -19,7 +19,7 @@ class Disponibilidad
     private $id;
 
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="integer", nullable=false)
      */
     private $disponibilidad;
 
--- src/Form/DisponibilidadType.php
+++ src/Form/DisponibilidadType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Disponibilidad;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,16 @@ class DisponibilidadType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('disponibilidad')
-            ->add('lugar')
+            ->add('disponibilidad', IntegerType::class,
+                [
+                    'attr' => ['min'=>1,'placeholder'=>'PLACEHOLDER','name' => 'Disponibilidad', 'title' => 'Disponibilidad', 'class' => 'form-control', 'id' => 'Disponibilidad'],
+                    'required' => true,
+                ])
+            ->add('lugar', EntityType::class,
+                [
+                    'attr' => ['name' => 'Lugar','title' => 'Lugar', 'class' => 'form-control','id' => 'lugar'],
+                    'class' => 'App\Entity\LugarDeRealizacion',
+                ])
         ;
     }
 
